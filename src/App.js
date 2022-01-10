4ca0bde72790a6382813a6ca077c484afa3dec3e
karthik <karthikgeo4u@gmail.com>
2022-01-10 11:25:57 +0530

    updtaed menu items to navigate withRouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
-// import React, { Component } from 'react';
+
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
-function App() {
-  return <div className="App">
-    <HomePage />
+const HelmetPage = () => (
+  <div>
+    <h1> HELMETS PAGE </h1>
   </div>
+);
+
+
+function App() {
+
+  return (
+    <div>
+      <Switch>
+      
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/helmets' component={HelmetPage} />
+        
+      </Switch>
+    </div>
+  );
 }
-export default App;
+export default App;;
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,63 +1,62 @@
-import React from 'react'
+import React from 'react';
 
 import MenuItem from '../menu-item/menu-item.component';
 
 import './directory.styles.scss';
 
 class Directory extends React.Component {
-    constructor(){
+    constructor() {
         super();
 
         this.state = {
             sections: [
                 {
-                title: 'helmets',
-                imageUrl: 'https://images-eu.ssl-images-amazon.com/images/I/51bKw-rw5gL._SR600%2C315_PIWhiteStrip%2CBottomLeft%2C0%2C35_SCLZZZZZZZ_FMpng_BG255%2C255%2C255.jpg',
-                
-                id: 1
-            },
-
-            {
-                title: 'jackets',
-                imageUrl: 'https://www.customelements.in/wp-content/uploads/2021/12/LS2-Teide-Men-All-Season-Black-Riding-Jacket-4.jpg',
-                id: 2
-            },
-
-            {
-                title: 'gloves',
-                imageUrl: 'https://5.imimg.com/data5/FS/IQ/MY-72777223/winter-protection-pro-biker-gloves-500x500.jpg',
-                id: 3
-            },
-
-            {
-                title: 'boots',
-                imageUrl: 'https://autocart.biz/wp-content/uploads/imgp/Puma-250-3-4869.jpg',
-                size: 'large',
-                id: 4
-            },
-
-            {
-                title: 'accessories',
-                imageUrl: 'https://i.ytimg.com/vi/tx4SfuB2Suw/maxresdefault.jpg',
-                size: 'large',
-                id: 5
-            }     
-        ],
-        }
+                    title: 'helmets',
+                    imageUrl: 'https://images-eu.ssl-images-amazon.com/images/I/51bKw-rw5gL._SR600%2C315_PIWhiteStrip%2CBottomLeft%2C0%2C35_SCLZZZZZZZ_FMpng_BG255%2C255%2C255.jpg',
+                    id: 1,
+                    linkUrl: 'helmets'
+                },
+                {
+                    title: 'jackets',
+                    imageUrl: 'https://www.customelements.in/wp-content/uploads/2021/12/LS2-Teide-Men-All-Season-Black-Riding-Jacket-4.jpg',
+                    id: 2,
+                    linkUrl: ''
+                },
+                {
+                    title: 'gloves',
+                    imageUrl: 'https://5.imimg.com/data5/FS/IQ/MY-72777223/winter-protection-pro-biker-gloves-500x500.jpg',
+                    id: 3,
+                    linkUrl: ''
+                },
+                {
+                    title: 'boots',
+                    imageUrl: 'https://autocart.biz/wp-content/uploads/imgp/Puma-250-3-4869.jpg',
+                    size: 'large',
+                    id: 4,
+                    linkUrl: ''
+                },
+                {
+                    title: 'accessories',
+                    imageUrl: 'https://i.ytimg.com/vi/tx4SfuB2Suw/maxresdefault.jpg',
+                    size: 'large',
+                    id: 5,
+                    linkUrl: ''
+                }
+            ]
+        };
     }
 
     render() {
-        return(
+        return (
             <div className='directory-menu'>
                 {
-                    this.state.sections.map(({title, imageUrl, id, size}) => (
-                        <MenuItem key ={id} title={title} imageUrl={imageUrl} size={size} />
+                    this.state.sections.map(({ id, ...otherSectionProps }) => (
+                        <MenuItem key={id} {...otherSectionProps}/>
                     ))
                 }
             </div>
-
-        )
+        );
     }
 }
 
-export default Directory;  
+export default Directory;
